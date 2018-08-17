06f77388dd07779734701813b4f363a736068559
JaimeVillanueva <31341581+JaimeVillanueva@users.noreply.github.com>
2018-08-17 18:50:45 -0400

    k-nearest neighbor analysis

---
+++ knn1.R
@@ -0,0 +1,65 @@
+#https://www.r-bloggers.com/k-nearest-neighbor-step-by-step-tutorial/
+
+set.seed(12345)
+
+library(caret)
+library(e1071)
+library(ROCR)
+
+#data <- read.csv("data/BreastCancer_titled_ed.csv", header = TRUE)
+#dataID <- data[,1]
+#data <- data[,2:11]
+
+#index = createDataPartition(data$Outcome, p = 0.7, list = F )
+#train = data[index,]
+#validation = data[-index,]
+
+train <-read.csv("data/breast_cancer_training.csv", header = TRUE)
+validation <- read.csv("data/breast_cancer_testing.csv", header = TRUE)
+train <- train[,2:11]
+train_outcome <- sapply(train$Outcome, function(x) if (x > 3) {"M"} else {"B"})
+train <- cbind(train, train_outcome)
+train <- train[,-10]
+validation <- validation[,2:11]
+validation_outcome <- sapply(validation$Outcome, function(x) if (x > 3) {"M"} else {"B"})
+validation <- cbind(validation, validation_outcome)
+validation <- validation[,-10]
+
+dim(train)
+dim(validation)
+names(train)
+head(train)
+head(validation)
+
+trc = trainControl(method = "repeatedcv",
+                 number = 10, # k-fold
+                 repeats = 3,
+                 classProbs = TRUE,
+                 summaryFunction = twoClassSummary)
+
+model1 <- train(train_outcome~. , data = train, method = "knn",
+                trControl = trc,
+                metric = "ROC",
+                tuneLength = 10)
+
+model1
+plot(model1)
+
+# Validation
+valid_pred <- predict(model1, validation, type = "prob")
+valid_pred2 <- predict(model1, newdata=validation)
+
+
+#Storing Model Performance Scores using library ROCR
+predicted_values <-prediction(valid_pred[,2],validation$validation_outcome)
+
+# AUC
+perf_val <- performance(predicted_values, "auc")
+perf_val
+
+# Plot AUC 
+perf_val <- performance(predicted_values, "tpr", "fpr")
+plot(perf_val, col = "green", lwd = 1.5)
+
+#Confusion Matrix
+confusionMatrix(valid_pred2, validation$validation_outcome)
